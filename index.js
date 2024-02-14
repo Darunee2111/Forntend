12f71a255b7c5796e669e0eb7d1f0b904a5f0e37
Darunee2111 <6406021610011@fitm.kmutnb.ac.th>
2024-02-14 10:51:22 +0700

    save

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 var bodyParser = require("body-parser");
 const app = express();
+const path = require("path");
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
@@ -10,7 +11,7 @@ const base_url = "http://node58674-darunee-noderest2.proen.app.ruk-com.cloud";
 ////
 
 // Set the template engine
-//app.set("views", path.join(__dirname, "/public/views"));
+app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
