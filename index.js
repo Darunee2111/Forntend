ab05903cace4370385eb7a673b1546c76acaacb6
Darunee2111 <s6406021610011@email.kmutnb.ac.th>
2024-02-14 09:50:33 +0700

    f

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 // Base URL for the API
 //const base_url = "https://api.example.com";
 //const base_url = "http://localhost:3000";
-const base_url = "http://node58674-darunee-noderest2.proen.app.ruk-com.cloud";
+//const base_url = "http://node58674-darunee-noderest2.proen.app.ruk-com.cloud";
 
 
 // Set the template engine
